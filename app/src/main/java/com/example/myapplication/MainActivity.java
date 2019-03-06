00cbacc867b0eeb46c60234cc6529bd0f7ce24ad
mwebe <mwebembezi@gmail.com>
2019-03-06 08:38:07 +0300

    Cafe App

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+//Mwebembezi Eric
+//2017/BIT/109/ps
 
 public class MainActivity extends AppCompatActivity {
 
